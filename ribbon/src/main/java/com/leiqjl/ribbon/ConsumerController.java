6d27f5825caf4ed8990b3a0b6032a98527f4391b
leiqjl <leiqjl@gmail.com>
2019-02-26 19:37:26 +0800

    add Hystrix

--- ribbon/src/main/java/com/leiqjl/ribbon/ConsumerController.java
+++ ribbon/src/main/java/com/leiqjl/ribbon/ConsumerController.java
@@ -3,17 +3,16 @@ package com.leiqjl.ribbon;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private ConsumerService consumerService;
 
     @RequestMapping("/consumer")
-    public String test() {
-        return restTemplate.getForEntity("http://hello-service/hello", String.class).getBody();
+    public String consumer() {
+        return consumerService.consumer();
     }
 
 }
