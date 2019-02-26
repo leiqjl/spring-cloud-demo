c54a90edd7e668581af564c8778be511d0339631
leiqjl <leiqjl@gmail.com>
2019-02-26 16:54:02 +0800

    ribbon

--- ribbon/src/main/java/com/leiqjl/ribbon/ConsumerController.java
+++ ribbon/src/main/java/com/leiqjl/ribbon/ConsumerController.java
@@ -1,24 +1,19 @@
 package com.leiqjl.ribbon;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.URI;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
 
     @Autowired
-    private LoadBalancerClient loadBalancerClient;
+    private RestTemplate restTemplate;
 
     @RequestMapping("/consumer")
     public String test() {
-        ServiceInstance serviceInstance = loadBalancerClient.choose("providers");
-        URI uri = URI.create(String.format("http://%s:%s", serviceInstance.getHost(), serviceInstance.getPort()));;
-        return uri.toString();
+        return restTemplate.getForEntity("http://hello-service/hello", String.class).getBody();
     }
 
 }
--- ribbon/src/main/java/com/leiqjl/ribbon/RibbonApplication.java
+++ ribbon/src/main/java/com/leiqjl/ribbon/RibbonApplication.java
@@ -1,12 +1,13 @@
 package com.leiqjl.ribbon;
 
-import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-
+@EnableEurekaClient
 @SpringBootApplication
 public class RibbonApplication {
 
@@ -15,8 +16,9 @@ public class RibbonApplication {
     }
 
     @Bean
-    public IRule ribbonRule() {
-        return new RandomRule();
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
