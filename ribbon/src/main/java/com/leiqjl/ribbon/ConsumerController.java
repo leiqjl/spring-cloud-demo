2e8703a8cc4c8eec9964f9b32745d397cea16747
leiqjl <leiqjl@gmail.com>
2019-02-26 20:23:30 +0800

    async

--- ribbon/src/main/java/com/leiqjl/ribbon/ConsumerController.java
+++ ribbon/src/main/java/com/leiqjl/ribbon/ConsumerController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+
 @RestController
 public class ConsumerController {
 
@@ -15,9 +17,18 @@ public class ConsumerController {
         return consumerService.consumer();
     }
 
+    @RequestMapping("/async")
+    public String async() throws ExecutionException, InterruptedException {
+        return consumerService.async();
+    }
 
     @RequestMapping("/command")
     public String command() {
         return consumerService.command();
+    }
+
+    @RequestMapping("/commandAsync")
+    public String commandAsync() throws ExecutionException, InterruptedException {
+        return consumerService.commandAsync();
     }
 }
--- ribbon/src/main/java/com/leiqjl/ribbon/ConsumerService.java
+++ ribbon/src/main/java/com/leiqjl/ribbon/ConsumerService.java
@@ -1,9 +1,13 @@
 package com.leiqjl.ribbon;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.command.AsyncResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 @Service
 public class ConsumerService {
@@ -19,10 +23,31 @@ public class ConsumerService {
         return "fail";
     }
 
+    @HystrixCommand(fallbackMethod = "testFallback")
+    public String async() throws ExecutionException, InterruptedException {
+        Future<String> future = new AsyncResult<String>() {
+            @Override
+            public String invoke() {
+                return restTemplate.getForEntity("http://hello-service/hello", String.class).getBody();
+            }
+
+            @Override
+            public String get() throws UnsupportedOperationException {
+                return invoke();
+            }
+        };
+        return future.get();
+    }
 
 
     public String command() {
         ConsumerCommand command = new ConsumerCommand("command", restTemplate);
         return command.execute();
+    }
+
+    public String commandAsync() throws ExecutionException, InterruptedException {
+        ConsumerCommand command = new ConsumerCommand("commandAsync", restTemplate);
+        Future<String> future = command.queue();
+        return future.get();
     }
 }
